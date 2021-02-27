5240f53e0692343b199c161508f4711b86459f86
edaozcan <73548267+edaozcan@users.noreply.github.com>
2021-02-28 01:52:51 +0300

    27.02.2021

---
+++ Business/DependencyResolvers/Autofac/AutofacBussinessModule.cs
@@ -0,0 +1,20 @@
+﻿using Autofac;
+using Business.Abstract;
+using Business.Concrete;
+using DateAccess.Abstract;
+using DateAccess.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.DependencyResolvers.Autofac
+{
+     public class AutofacBussinessModule:Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<ProductManager>().As<IProductService>().SingleInstance();
+            builder.RegisterType<EfProductDal>().As<IProductDal>().SingleInstance();
+        }
+    }
+}
